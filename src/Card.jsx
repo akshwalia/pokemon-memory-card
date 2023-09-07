a850c8aecbbc280ccaad09575acd423cb9cfded1
Aksh Walia <akshwalia20@gmail.com>
2023-09-08 00:27:29 +0530

    Minor changes

--- src/Card.jsx
+++ src/Card.jsx
@@ -40,6 +40,7 @@ export default function Card({ updateScore, updateHighScore, changeGameOver, pok
         clickedCards=[];
     }, [reset]);
 
+
     function handleClick(e) {
         if (!clickedCards.includes(e.currentTarget.id)) {
             clickedCards.push(e.currentTarget.id);
